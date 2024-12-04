8065e21dc9a4f61097751d3735cb745194240030
ksan <aquam41@gmail.com>
2024-12-04 15:19:19 +0900

    [Feat, Test] 기본 JPA 레포지토리 작성 및 테스트코드 작성

---
+++ src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -0,0 +1,42 @@
+package study.datajpa.repository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+import study.datajpa.entity.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class MemberJpaRepository {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public Member save(Member member) {
+        entityManager.persist(member);
+        return member;
+    }
+
+    public Member find(Long id) {
+        return entityManager.find(Member.class, id);
+    }
+
+    public void delete(Member member) {
+        entityManager.remove(member);
+    }
+
+    public List<Member> findAll() {
+        return entityManager.createQuery("select m from Member m", Member.class).getResultList();
+    }
+
+    public Optional<Member> findById(Long id) {
+        Member member = entityManager.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+
+    public long count() {
+        return entityManager.createQuery("select count(m) from Member m", Long.class).getSingleResult();
+    }
+}
