3b558f1367e2321b4e76939ebb2416e7424977a6
ksan <aquam41@gmail.com>
2024-12-31 16:10:47 +0900

    [Feat, Test] nativeQuery 추가 및 테스트

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -282,4 +282,27 @@ class MemberRepositoryTest {
             System.out.println("usernameOnly = " + usernameOnly.getUsername() + " , " + usernameOnly.getTeam().getName());
         }
     }
+
+    @Test
+    public void nativeQuery() {
+        Team teamA = new Team("teamA");
+        em.persist(teamA);
+
+        Member m1 = new Member("m1", 0, teamA);
+        Member m2 = new Member("m2", 0, teamA);
+        em.persist(m1);
+        em.persist(m2);
+
+        em.flush();
+        em.clear();
+
+        Page<MemberProjection> result = memberRepository.findByNativeProjection(PageRequest.of(0, 10));
+        List<MemberProjection> content = result.getContent();
+
+        for (MemberProjection m : content) {
+            System.out.println("memberProjection = " + m.getUsername());
+            System.out.println("memberProjection = " + m.getTeamName());
+        }
+
+    }
 }
