158507bdf66294c3e7f3c75748ca167c39ed3b67
ksan <aquam41@gmail.com>
2024-12-31 16:11:16 +0900

    [Chore] QueryDSL 설정 추가 및 테스트

--- src/test/java/study/datajpa/DatajpaApplicationTests.java
+++ src/test/java/study/datajpa/DatajpaApplicationTests.java
@@ -1,13 +1,35 @@
 package study.datajpa;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.entity.Hello;
+import study.datajpa.entity.QHello;
 
 @SpringBootTest
+@Transactional
 class DatajpaApplicationTests {
+
+    @PersistenceContext
+    EntityManager em;
 
     @Test
     void contextLoads() {
+        Hello hello = new Hello();
+        em.persist(hello);
+
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QHello qHello = new QHello("h");
+
+        Hello result = query
+                    .selectFrom(qHello)
+                    .fetchOne();
+        Assertions.assertEquals(result, hello);
+        Assertions.assertEquals(result.getId(), hello.getId());
     }
 
 }
